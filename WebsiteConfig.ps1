3bd1d92492d2ca1d5c8165d983cd6d2f029c04e8
Beth Cooper <ecoop@seas.upenn.edu>
2015-04-29 13:00:52 -0700

    Adding simple config file for demo

---
+++ WebsiteConfig.ps1
@@ -0,0 +1,12 @@
+﻿Configuration WebsiteConfig
+{
+    Node ("localhost")
+    {
+        # Install the Web Server role
+        WindowsFeature IIS
+        {
+            Ensure = "Absent"
+            Name = "Web-Server"
+        }
+    }
+} 
